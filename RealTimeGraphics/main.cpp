e3ab0bb94a253f6a307678e69c08d0c34a41f911
Yu Xi Lee <yuxilee@hotmail.com>
2024-01-24 12:06:14 -0500

    Initial Commit

---
+++ RealTimeGraphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RealTimeGraphics
+//
+//  Created by yuxi01px2018 on 24/1/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
